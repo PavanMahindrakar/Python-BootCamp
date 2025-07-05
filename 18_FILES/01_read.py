5b7ba46012d3009e9c5892c035213a89cdd8a833
PavanMahindrakar <pdmahindrakar@gmail.com>
2025-07-05 23:43:17 +0530

    initial commit

--- 18_FILES/01_read.py
+++ 18_FILES/01_read.py
@@ -1,4 +1,10 @@
+# This code reads the content of a file named "demo.txt" and prints it to the console.
+
 f= open("demo.txt", "r")
+
 content = f.read()
 print(content)
+
+# To close the file after reading, we need to call f.close()
+# If we don't close the file, it may lead to memory leaks or file corruption.
 f.close()

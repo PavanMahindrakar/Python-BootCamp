28ccfe5dadb6e134aedaef885b197a327aebd588
PavanMahindrakar <pdmahindrakar@gmail.com>
2025-07-08 23:47:12 +0530

    initial commit

--- 19_EXTERNAL_Modules/01_env.py
+++ 19_EXTERNAL_Modules/01_env.py
@@ -1,1 +1,10 @@
 import numpy as np
+
+# to create an virtual environment
+# python -m venv env
+# to activate the virtual environment
+# .\env\Scripts\activate
+# to install a package in the virtual environment
+# pip install requests
+# to deactivate the virtual environment
+# deactivate
